775d67aca2fb8c92a0030ed90526d27d53419607
mbtec22 <mbtec22@MacBook-Air-de-mbtec22-10.local>
2020-05-28 14:43:26 -0500

    first commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by mbtec22 on 5/28/20.
+//  Copyright © 2020 mbtec22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
